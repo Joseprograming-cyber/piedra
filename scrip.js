6dd544198ce4a796deb6f1c778f3a94b1065d537
Joseprograming-cyber <janco2306@gmail.com>
2025-10-21 13:23:58 -0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,20 @@
+//variables
+//no pueden empezar con numero ni caracteres especiales
+//notacion es el tipo de forma en la escribe variables , funciones
+//camelcase, pascal case, snake_case
+//las funciones no tienen comas 
+//let and const se puede definir variables ,< const es para objetos
+
+alert("vamos a jugar a piedra papel o tijera")
+let jugador= prompt("cual escoger? 🪨🧻✂️")
+jugador= 1
+
+//condciones
+if (jugador === "🪨" || jugador==="🧻" || jugador === "✂️"){
+   alert("a jugar")
+}else{
+   alerta("escribiste una opcion invalida 🤦‍♂️")
+}
+
+
+
